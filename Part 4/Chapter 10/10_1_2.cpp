a87dab27cb20d722b9795888cfcd527a9123c6ee
rluver <skyd0608@gmail.com>
2022-01-04 13:52:02 +0900

    file: Upload 10_1_2.cpp

---
+++ Part 4/Chapter 10/10_1_2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+class Point
+{
+private:
+	int xpos, ypos;
+	
+public:
+	Point(int x = 0, int y = 0) : xpos(x), ypos(y)
+	{}
+
+	void ShowPosition() const
+	{
+		cout << '[' << xpos << ", " << ypos << ']' << endl;
+	}
+
+	Point operator+=(const Point& ref)
+	{
+		xpos += ref.xpos;
+		ypos += ref.ypos;
+
+		return *this;
+	}
+
+	Point operator-=(const Point& ref)
+	{
+		xpos -= ref.xpos;
+		ypos -= ref.ypos;
+
+		return *this;
+	}
+
+};
+
+int main()
+{
+	Point pos1(20, 30);
+	Point pos2(5, 7);
+
+	(pos1 += pos2).ShowPosition();
+	(pos1 -= pos2).ShowPosition();
+}
